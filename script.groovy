12f3cfa984e719f7f42c3e9be161e915afa2bf60
SewarDrawhe <45765243+SewarDrawhe@users.noreply.github.com>
2021-09-30 09:18:58 +0300

    updated

--- script.groovy
+++ script.groovy
@@ -1,1 +1,12 @@
-
+def build(){
+     echo 'building the application...'
+}
+def test(){
+  echo 'testing the application...'
+}
+def deploy(){
+  
+   echo 'deplying the application...'
+   echo "deploying version ${params.VERSION}"
+}
+return this
